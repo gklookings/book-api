f1883e83481c5dee7dfd026df739d5ba601baf21
Varun <varun@lookings.in>
2024-05-15 13:50:47 +0530

    openai version update

--- app/langchain/get_answer.py
+++ app/langchain/get_answer.py
@@ -16,49 +16,55 @@ from langchain_core.prompts import PromptTemplate
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 
 def get_answer(question):
-  # Load, chunk and index the contents.
-  loader = PyPDFLoader("app/langchain/books/crime-and-punishment.pdf")
-  docs = loader.load_and_split()
+  try:
+    # Load, chunk and index the contents.
+    loader = PyPDFLoader("app/langchain/books/crime-and-punishment.pdf")
+    docs = loader.load_and_split()
 
-  # This text splitter is used to create the parent documents
-  parent_splitter = RecursiveCharacterTextSplitter(chunk_size=5000)
-  # This text splitter is used to create the child documents
-  # It should create documents smaller than the parent
-  child_splitter = RecursiveCharacterTextSplitter(chunk_size=800)
-  # The vectorstore to use to index the child chunks
-  vectorstore = Chroma(
-        collection_name="split_parents", 
-        embedding_function=OpenAIEmbeddings()
-  )
-  # The storage layer for the parent documents
-  store = InMemoryStore()
-  retriever = ParentDocumentRetriever(
-      vectorstore=vectorstore,
-      docstore=store,
-      child_splitter=child_splitter,
-      parent_splitter=parent_splitter,
-  )
-  retriever.add_documents(docs)
+    # This text splitter is used to create the parent documents
+    parent_splitter = RecursiveCharacterTextSplitter(chunk_size=5000)
+    # This text splitter is used to create the child documents
+    # It should create documents smaller than the parent
+    child_splitter = RecursiveCharacterTextSplitter(chunk_size=800)
+    # The vectorstore to use to index the child chunks
+    vectorstore = Chroma(
+          collection_name="split_parents", 
+          embedding_function=OpenAIEmbeddings()
+    )
+    # The storage layer for the parent documents
+    store = InMemoryStore()
+    retriever = ParentDocumentRetriever(
+        vectorstore=vectorstore,
+        docstore=store,
+        child_splitter=child_splitter,
+        parent_splitter=parent_splitter,
+    )
+    retriever.add_documents(docs)
 
-  def format_docs(docs):
-      return "\n\n".join(doc.page_content for doc in docs)
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+    
+
+    template = """Use the following pieces of context and the broader conext of the document or the story to answer the question at the end.
+    Answer the question in detail.
+
+    {context}
+
+    Question: {question} """
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": retriever|format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    answer = rag_chain.invoke(question)
+
+    return answer
   
-
-  template = """Use the following pieces of context and the broader conext of the document or the story to answer the question at the end.
-  Answer the question in detail.
-
-  {context}
-
-  Question: {question} """
-  custom_rag_prompt = PromptTemplate.from_template(template)
-
-  rag_chain = (
-      {"context": retriever|format_docs, "question": RunnablePassthrough()}
-      | custom_rag_prompt
-      | llm
-      | StrOutputParser()
-  )
-
-  answer = rag_chain.invoke(question)
-
-  return answer
+  except Exception as e:
+      # Handle the exception gracefully
+      print(f"An error occurred: {e}")
+      return "An error occured. Please try again"  # Or return an appropriate value indicating failure
