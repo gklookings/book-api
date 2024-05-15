41040207b28acd42f010d3a2879618f0d6f208ba
Varun <varun@lookings.in>
2024-05-15 20:07:45 +0530

    store persistor

--- app/langchain/get_answer.py
+++ app/langchain/get_answer.py
@@ -1,3 +1,4 @@
+import os.path
 import bs4
 from langchain import hub
 from langchain_community.document_loaders import (
@@ -12,6 +13,17 @@ from langchain_openai import ChatOpenAI
 from langchain.retrievers import ParentDocumentRetriever
 from langchain.storage import InMemoryStore
 from langchain_core.prompts import PromptTemplate
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("output.txt")
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 
@@ -19,27 +31,39 @@ def get_answer(question):
   try:
     # Load, chunk and index the contents.
     loader = PyPDFLoader("app/langchain/books/crime-and-punishment.pdf")
-    docs = loader.load_and_split()
+    documents = loader.load_and_split()
 
     # This text splitter is used to create the parent documents
     parent_splitter = RecursiveCharacterTextSplitter(chunk_size=5000)
+    docs = parent_splitter.split_documents(documents)
     # This text splitter is used to create the child documents
     # It should create documents smaller than the parent
     child_splitter = RecursiveCharacterTextSplitter(chunk_size=800)
     # The vectorstore to use to index the child chunks
-    vectorstore = Chroma(
-          collection_name="split_parents", 
-          embedding_function=OpenAIEmbeddings()
-    )
+    PERSIST_DIR = "./chroma_db"
+    if not os.path.exists(PERSIST_DIR):
+      embedding_function = OpenAIEmbeddings()
+      vectorstore = Chroma.from_documents(
+            docs,
+            embedding_function,
+            persist_directory=PERSIST_DIR
+      )
+    else:
+        vectorstore = Chroma(
+          persist_directory=PERSIST_DIR,
+          embedding_function=OpenAIEmbeddings(),
+      )
     # The storage layer for the parent documents
-    store = InMemoryStore()
-    retriever = ParentDocumentRetriever(
-        vectorstore=vectorstore,
-        docstore=store,
-        child_splitter=child_splitter,
-        parent_splitter=parent_splitter,
-    )
-    retriever.add_documents(docs)
+    # store = InMemoryStore()
+    # retriever = ParentDocumentRetriever(
+    #     vectorstore=vectorstore,
+    #     docstore=vectorstore,
+    #     child_splitter=child_splitter,
+    #     parent_splitter=parent_splitter,
+    # )
+    # retriever.add_documents(docs)
+
+    retriever=vectorstore.as_retriever(search_type="mmr")
 
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
@@ -66,5 +90,5 @@ def get_answer(question):
   
   except Exception as e:
       # Handle the exception gracefully
-      print(f"An error occurred in langchain: {e}")
+      logger.error(f"An error occurred: {e}", exc_info=True)
       return "An error occured. Please try again"  # Or return an appropriate value indicating failure
