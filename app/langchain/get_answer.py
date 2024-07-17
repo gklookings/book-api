7ff443a94d27c06fb7c2448f048cd4520e36c4b3
Varun <varun@lookings.in>
2024-07-17 18:12:09 +0530

    full poems

--- app/langchain/get_answer.py
+++ app/langchain/get_answer.py
@@ -59,11 +59,14 @@ loaders.append(docLists(Docx2txtLoader("app/langchain/books/motanabi-15-all.docx
 loaders.append(docLists(PyPDFLoader("app/langchain/books/motanabi-timeline.pdf"),120))
 loaders.append(docLists(Docx2txtLoader("app/langchain/books/motanabi-poems-all.docx"),121))
 loaders.append(docLists(PyPDFLoader("app/langchain/books/26-1.pdf"),122))
-loaders.append(docLists(TextLoader("app/langchain/books/Ex-mot1.txt"),123))
-loaders.append(docLists(TextLoader("app/langchain/books/Ex-mot2.txt"),124))
-loaders.append(docLists(TextLoader("app/langchain/books/Ex-mot3.txt"),125))
-loaders.append(docLists(TextLoader("app/langchain/books/Ex-mot4.txt"),126))
 
+
+for i in range(1, 325):  # From 1 to 324 inclusive
+    filename = f"Ex-mot{i}.txt"  # Construct the filename
+    book_id = 123 + i - 1  # Compute the ID
+    loaders.append(docLists(TextLoader(f"app/langchain/books/{filename}"), book_id))
+
+#vector store for docs other than poems
 def create_vector_store_for_document(loader, doc_id):
     documents = loader.load_and_split()
     doc_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
@@ -85,19 +88,22 @@ def create_vector_store_for_document(loader, doc_id):
 
     return vectorstore
 
+#vector store for poems
 def create_combined_vector_store():
     all_docs = []
     doc_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     
     for loader in loaders:
-        if loader.id in {123, 124, 125, 126}:
+        if 123 <= loader.id <= 324:  # Check if the id is within the range 123 to 324
             documents = loader.name.load_and_split()
             docs = doc_splitter.split_documents(documents)
             all_docs.extend(docs)
 
+
     PERSIST_DIR = "./chroma_db/combined"
+    embedding_function = OpenAIEmbeddings()
+
     if not os.path.exists(PERSIST_DIR):
-        embedding_function = OpenAIEmbeddings()
         vectorstore = Chroma.from_documents(
             all_docs,
             embedding_function,
@@ -106,74 +112,13 @@ def create_combined_vector_store():
     else:
         vectorstore = Chroma(
             persist_directory=PERSIST_DIR,
-            embedding_function=OpenAIEmbeddings(),
+            embedding_function=embedding_function,
         )
 
     return vectorstore
 
-# def openAIFileSearch(question):
 
-#     assistant = client.beta.assistants.create(
-#     name="Poem Analyst Assistant",
-#     instructions="You are an expert poem analyst. Use you knowledge base to answer questions about poems and their contents based on the files provided..",
-#     model="gpt-4o",
-#     tools=[{"type": "file_search"}],
-#     )
-
-#     # Check if the vector store exists or create a new one
-#     vector_store_name = "Poems"
-#     vector_store = client.beta.vector_stores.create(name=vector_store_name)
-    
-#     # Ready the files for upload to OpenAI
-#     file_paths = ["app/langchain/books/CrimePunishment.docx"]
-#     file_streams = [open(path, "rb") for path in file_paths]
-    
-#     # Use the upload and poll SDK helper to upload the files, add them to the vector store,
-#     # and poll the status of the file batch for completion.
-#     file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-#     vector_store_id=vector_store.id, files=file_streams
-#     )
-
-#     assistant = client.beta.assistants.update(
-#     assistant_id=assistant.id,
-#     tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
-#     )
-
-#     # Create a thread
-#     thread = client.beta.threads.create(
-#     messages=[
-#         {
-#         "role": "user",
-#         "content": question,
-#         }
-#     ]
-#     )
-
-#     run = client.beta.threads.runs.create_and_poll(
-#     thread_id=thread.id, assistant_id=assistant.id
-#     )
-
-#     messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
-
-#     message_content = messages[0].content[0].text
-#     annotations = message_content.annotations
-#     citations = []
-#     for index, annotation in enumerate(annotations):
-#         message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
-#         if file_citation := getattr(annotation, "file_citation", None):
-#             cited_file = client.files.retrieve(file_citation.file_id)
-#             citations.append(f"[{index}] {cited_file.filename}")
-
-#     response = {
-#         "answer":message_content.value,
-#         "context":[{"page_content":"\n".join(citations)}],
-#         "quesiton":question
-#     }
-
-#     return response
-
-
-
+#get answer function
 def get_answer(question,bookId):
     try:
         if bookId in {127}:
@@ -184,7 +129,7 @@ def get_answer(question,bookId):
                     vectorstore = create_vector_store_for_document(loader.name, bookId)
                     break
           
-        retrievers = vectorstore.as_retriever(search_kwargs={"k": 100})
+        retrievers = vectorstore.as_retriever(search_kwargs={"k": 500})
 
         system_prompt = (
             "Use the given context to answer the question. "
