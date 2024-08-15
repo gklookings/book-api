b780d8ca38d3feec3010fc6c4ba4633442a0da01
Varun <varun@lookings.in>
2024-08-15 16:01:44 +0530

    port change

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == "__main__":
-  uvicorn.run("app.server.api:app", host="0.0.0.0", port=4000, reload=True)
+  uvicorn.run("app.server.api:app", host="0.0.0.0", port=6000, reload=True)
